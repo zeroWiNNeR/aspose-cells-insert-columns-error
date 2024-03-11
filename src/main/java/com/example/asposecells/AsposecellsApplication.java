a931fff7a5b6c9d7e02cd7f0972a74a3c19cdb07
ayvekovshinin <ayvekovshinin@asuproject.ru>
2024-03-11 08:36:06 +0300

    aspose-cells-copy-range-unexpected-behavior

--- src/main/java/com/example/asposecells/AsposecellsApplication.java
+++ src/main/java/com/example/asposecells/AsposecellsApplication.java
@@ -10,22 +10,32 @@ public class AsposecellsApplication {
 		try (var is = AsposecellsApplication.class.getResourceAsStream("/example_copy_validation_range.xlsx")) {
 			Workbook workbook = new Workbook(is);
 			Worksheet worksheet = workbook.getWorksheets().get(0);
+			Cells cells = worksheet.getCells();
 
-			/*Range range = worksheet.getCells().createRange(0, 1, 1, 2);
-			Range copyRange = worksheet.getCells().createRange(0, 3, 1, 2);
+			cells.insertColumns(4, 1, true);
+
+			System.out.printf("B. text1: '%s', %s, validation2: '%s'%n", cells.get("B1").getValue(), cells.get("B2").getStyle().getForegroundColor(), cells.get("B2").getValidation());
+			System.out.printf("C. text1: '%s', %s, validation2: '%s'%n", cells.get("C1").getValue(), cells.get("C2").getStyle().getForegroundColor(), cells.get("C2").getValidation());
+			System.out.printf("D. text1: '%s', %s, validation2: '%s'%n", cells.get("D1").getValue(), cells.get("D2").getStyle().getForegroundColor(), cells.get("D2").getValidation());
+			System.out.printf("E. text1: '%s', %s, validation2: '%s'%n", cells.get("E1").getValue(), cells.get("E2").getStyle().getForegroundColor(), cells.get("E2").getValidation());
+			System.out.printf("F. text1: '%s', %s, validation2: '%s'%n", cells.get("F1").getValue(), cells.get("F2").getStyle().getForegroundColor(), cells.get("F2").getValidation());
+			System.out.println("New column created");
+
+			Range range = cells.createRange("B1", "C2");
+			Range copyRange = cells.createRange("D1", "E2");
 
 			PasteOptions options = new PasteOptions();
 			options.setPasteType(PasteType.ALL);
-			copyRange.copy(range);*/
+			copyRange.copy(range);
 
-			worksheet.getCells().insertColumns(2, 2);
-
-			System.out.printf("B1. text: '%s', validation: '%s'%n", worksheet.getCells().get("B1").getValue(), worksheet.getCells().get("B2").getValidation());
-			System.out.printf("C1. text: '%s', validation: '%s'%n", worksheet.getCells().get("C1").getValue(), worksheet.getCells().get("C2").getValidation());
-			System.out.printf("D1. text: '%s', validation: '%s'%n", worksheet.getCells().get("D1").getValue(), worksheet.getCells().get("D2").getValidation());
-			System.out.printf("E1. text: '%s', validation: '%s'%n", worksheet.getCells().get("E1").getValue(), worksheet.getCells().get("E2").getValidation());
-			System.out.printf("F1. text: '%s', validation: '%s'%n", worksheet.getCells().get("F1").getValue(), worksheet.getCells().get("F2").getValidation());
+			System.out.printf("B. text1: '%s', %s, validation2: '%s'%n", cells.get("B1").getValue(), cells.get("B2").getStyle().getForegroundColor(), cells.get("B2").getValidation());
+			System.out.printf("C. text1: '%s', %s, validation2: '%s'%n", cells.get("C1").getValue(), cells.get("C2").getStyle().getForegroundColor(), cells.get("C2").getValidation());
+			System.out.printf("D. text1: '%s', %s, validation2: '%s'%n", cells.get("D1").getValue(), cells.get("D2").getStyle().getForegroundColor(), cells.get("D2").getValidation());
+			System.out.printf("E. text1: '%s', %s, validation2: '%s'%n", cells.get("E1").getValue(), cells.get("E2").getStyle().getForegroundColor(), cells.get("E2").getValidation());
+			System.out.printf("F. text1: '%s', %s, validation2: '%s'%n", cells.get("F1").getValue(), cells.get("F2").getStyle().getForegroundColor(), cells.get("F2").getValidation());
 			System.out.println("Copying completed");
+			workbook.save("result.xlsx");
+			System.out.println("Result saved");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
